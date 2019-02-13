879f8f1d6c77d2342ee71225c04ec1254734f291
Sven Klomp <mail@klomp.eu>
2019-02-13 21:42:11 +0100

    Initial commit

---
+++ index.js
@@ -0,0 +1,21 @@
+var app = require('express')();
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
+
+app.get('/', function(req, res){
+  res.sendFile(__dirname + '/index.html');
+});
+
+app.get('/receive', function(req, res){
+  res.sendFile(__dirname + '/receive.html');
+});
+
+io.on('connection', function(socket){
+  socket.on('phonenumber', function(msg){
+    io.emit('phonenumber', msg);
+  });
+});
+
+http.listen(3000, function(){
+  console.log('listening on *:3000');
+});

eff34c5f38e347d84ddb62a42641271a5f85ce8a
Sven Klomp <mail@klomp.eu>
2019-02-15 11:42:20 +0100

    Add message buffer

--- index.js
+++ index.js
@@ -49,17 +49,61 @@ io.on('connection', function(socket){
         socket.leave(socket.room);
 
     socket.room = room;
-    console.log("Joining room "+room);
+
+    var msg=getmsg(room);
+    if (msg) {
+      socket.emit('phonenumber', msg);
+    }
     
+    console.log("Joining room "+room);
     socket.join(room);
   });
   
   socket.on('phonenumber', function(phonenumber){
-    if (socket.room)
+    if (socket.room) {
+      addmsg(socket.room, phonenumber);
       io.sockets.to(socket.room).emit('phonenumber', phonenumber);
+    }
   });
 });
 
 http.listen(3000, function(){
   console.log('listening on *:3000');
 });
+
+
+var msgbuf = {}
+var max_age = 5*60*1000; // 5 minutes
+
+var addmsg = function(room, msg) {
+    msgbuf[room]={
+      msg: msg,
+      timestamp: Date.now()
+    };
+}
+
+var getmsg = function(room) {
+    if (msgbuf.hasOwnProperty(room)){
+      return msgbuf[room].msg;
+    }
+    else {
+      return null;
+    }
+}
+
+var cleanmsg = function() {
+  var timestamp=Date.now()-max_age; //15 seconds
+  for (var room in msgbuf) {
+    if (msgbuf.hasOwnProperty(room)) {
+      {
+        if (msgbuf[room].timestamp<timestamp) {
+          console.log("Removing "+ room);
+          delete msgbuf[room];
+        }
+      }
+    }
+  }
+}
+
+setInterval(cleanmsg, max_age);
+
